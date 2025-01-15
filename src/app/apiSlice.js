fcec8ceb2e2c7dbd7bb23c952eaed12c78fb2e9b
Es-Mes <esterleah085@gmail.com>
2025-01-15 22:17:53 +0200

    Start Design2

--- src/app/apiSlice.js
+++ src/app/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        // baseUrl: "http://localhost:50901",
-        baseUrl:"https://gizbar.onrender.com",
+        baseUrl: "http://localhost:50901",
+        // baseUrl:"https://gizbar.onrender.com",
         credentials: "include",
         prepareHeaders: (headers, { getState }) => {
             const token = getState().auth.token;
@@ -23,3 +23,28 @@ const apiSlice = createApi({
 
 export const { useGetAgentQuery } = apiSlice;
 export default apiSlice;
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// const apiSlice = createApi({
+//     reducerPath: "api",
+//     baseQuery: fetchBaseQuery({
+//         baseUrl: "http://localhost:10000",
+//         // baseUrl:"https://gizbar.onrender.com",
+//         credentials: "include",
+//         prepareHeaders: (headers, { getState }) => {
+//             const token = getState().auth.token;
+//             if (token) {
+//                 headers.set("authorization", `Bearer ${token}`);
+//             }
+//             return headers;
+//         },
+//     }),
+//     endpoints: (builder) => ({
+//         getAgent: builder.query({
+//             query: ({ phone }) => `/api/agent/${phone}`,
+//         }),
+//     }),
+// });
+
+// export const { useGetAgentQuery } = apiSlice;
+// export default apiSlice;
