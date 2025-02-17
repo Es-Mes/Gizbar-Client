8da66635b4591d650c5dc9463b436caede8b95cb
Es-Mes <esterleah085@gmail.com>
2025-02-17 12:40:12 +0200

    Customers transactions display

--- src/fetures/expenses/list/ExpensesList.js
+++ src/fetures/expenses/list/ExpensesList.js
@@ -1,21 +1,60 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import ExpensesItem from '../view/ExpensesItem';
+
+// const ExpensesList = ({ transactions }) => {
+
+//     if (transactions == []) {
+//         return <> אין עסקאות העונות על התנאים</>
+//     }
+
+//     return (
+//         <div>
+//             {transactions.map((transaction) => (
+//                 <ExpensesItem
+//                     key={transaction._id}
+//                     transaction={transaction}
+//                 />
+//             ))}
+//         </div>
+//     );
+// };
+
+// export default ExpensesList;
+import '../../transactions/list/TransactionList.css';
+import React, { useState } from "react";
 import ExpensesItem from '../view/ExpensesItem';
 
 const ExpensesList = ({ transactions }) => {
+    const [transactionsList, setTransactionsList] = useState(transactions);
 
-    if (transactions == []) {
-        return <> אין עסקאות העונות על התנאים</>
-    }
+    const handleUpdateTransaction = (updatedTransaction) => {
+        setTransactionsList(
+            transactionsList.map((t) =>
+                t._id === updatedTransaction._id ? updatedTransaction : t
+            )
+        );
+    };
 
     return (
-        <div>
-            {transactions.map((transaction) => (
-                <ExpensesItem
-                    key={transaction._id}
-                    transaction={transaction}
-                />
-            ))}
-        </div>
+        <table className="transaction-table">
+            <thead>
+                <tr>
+                    
+                    <th>סוג השירות</th>
+                    <th>על ידי</th>
+                    <th>מחיר</th>
+                    <th>סטטוס</th>   
+                </tr>
+            </thead>
+            <tbody>
+                {transactionsList.map((transaction) => (
+                    <ExpensesItem
+                        key={transaction._id}
+                        transaction={transaction}
+                    />
+                ))}
+            </tbody>
+        </table>
     );
 };
 
--- src/fetures/expenses/view/ExpensesItem.js
+++ src/fetures/expenses/view/ExpensesItem.js
@@ -1,60 +1,33 @@
-import React from 'react';
-import './expensesItem.css'
+import React from "react";
+import "../../transactions/view/TransactionItem.css"
+
+const alertsLevelMapping = {
+    once: "פעם אחת",
+    weekly: "שבועי",
+    nudnik: "נודניק",
+};
 
 const ExpensesItem = ({ transaction }) => {
-    const {
-        serviceName,
-        price,
-        status,
-        billingDay,
-        description,
-        agent,
-        alerts,
-        typeAlerts,
-        alertsLevel,
-    } = transaction;
-
-    const statusTranslations = {
-        notPaid: "לא שולם",
-        paid: "שולם",
-        pendingCharge: "ממתין לחיוב",
-        canceled: "בוטל",
-    };
-
-    const types_alerts = {
-        'phone only': 'טלפון בלבד',
-        'email only': 'מייל בלבד',
-        'email and phone': 'טלפון ומייל',
-        'human': 'תזכורת אנושית'
-    }
-
-    const alerts_level = {
-        once: 'פעם אחת',
-        weekly: 'שבועי',
-        nudnik: 'נודניק,'
-    }
 
     return (
-        <div className='transaction-card'>
-            <div className='transaction-header'>
-                <h3>{serviceName || "שירות ללא שם"}</h3>
-                <p className='expenses-price'>₪{price}</p>
-            </div>
-            <div className='transaction-body'>
-                <p><strong>סטטוס:</strong> {statusTranslations[status]}</p>
-                <p><strong>תאריך חיוב:</strong> {new Date(billingDay).toLocaleDateString('he-IL')}</p>
-                <p><strong>נותן שירות:</strong> {`${agent.first_name} ${agent.last_name}`} </p>
-                <p><strong>תיאור:</strong> {description}</p>
-                <p><strong>התראות:</strong> {alerts ? "כן" : "לא"}</p>
-                {alerts && (
-                    <div className='alerts-info'>
-                        <p><strong>סוג התראות:</strong> {types_alerts[typeAlerts]}</p>
-                        <p><strong>רמת התראות:</strong> {alerts_level[alertsLevel]}</p>
-                    </div>
-                )}
-            </div>
-        </div>
-
+        <tr>
+            <td>
+                {
+                    transaction.serviceName || "שירות ללא שם"
+                }
+            </td>
+            <td>{transaction.agent.first_name} {transaction.agent.last_name}</td>
+            <td>
+                {
+                    `₪${transaction.price}`
+                }
+            </td>
+            <td>
+                { transaction.status === "paid" ? "שולם" : "לא שולם"}
+            </td>
+          
+           
+        </tr>
     );
 };
 
