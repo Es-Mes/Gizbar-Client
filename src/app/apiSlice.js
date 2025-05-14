4acf67d1b3f0011ae00d8b9d0c1e3f60fd17e16c
Es-Mes <esterleah085@gmail.com>
2025-05-14 09:55:13 +0300

    Improving apiSlice with refreshToken

--- src/app/apiSlice.js
+++ src/app/apiSlice.js
@@ -4,11 +4,25 @@ const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl:process.env.REACT_APP_BASE_URL,
         credentials: "include",
-        prepareHeaders: (headers, { getState }) => {
+        prepareHeaders: async (headers, { getState,endpoint  }) => {
             const token = getState().auth.token;
             if (token) {
                 headers.set("authorization", `Bearer ${token}`);
             }
+            // אם יש בעיה עם ה־accessToken (למשל, פג תוקף), ננסה לרענן אותו
+            const response = await fetch('/api/auth/refresh', {
+                method: 'POST',
+                credentials: 'include', // חשוב שיתווסף כאן, כדי לשלוח את ה־cookie עם ה־refreshToken
+            });
+            if (response.ok) {
+                const data = await response.json();
+                if (data.accessToken) {
+                    // אם התקבל accessToken חדש, שמור אותו ב־state
+                    dispatch(setToken({ accessToken: data.accessToken }));
+                    headers.set("authorization", `Bearer ${data.accessToken}`);
+                }
+            }
+            
             return headers;
         },
     }),
